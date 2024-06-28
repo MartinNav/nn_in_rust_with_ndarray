6e89d57bbd51081127c33566fa697e95b08a8835
MartinNav <martinnavratilu@gmail.com>
2024-06-28 15:13:29 +0200

    added formating

--- src/activation.rs
+++ src/activation.rs
@@ -1,61 +1,52 @@
-
 pub struct Activation {
     pub function: fn(&f32) -> f32,
     pub derivation: fn(&f32) -> f32,
 }
 impl Activation {
     /// This function will instantiate softmax activation function
-    pub fn softmax()->Self{
-Activation {
-        function: |x: &f32| -> f32 {
-            if *x > 0. {
-                return *x;
-            } else {
-                0.
-            }
-        },
-        derivation: |x: &f32| -> f32 {
-            if *x > 0. {
-                return 1.0;
-            } else {
-                0.
-            }
-        },
-    }
-
-    }
-
-
-    /// x^3+1+(sin 9x)*0.1 function
-    pub fn experimental_x3()->Self{
-        let f = |x:&f32|->f32{
-            x.powi(3)+1.+f32::sin(9.*x)*0.1
-        };
-        let d = |x:&f32|->f32{
-            3.*x.powi(2)+f32::cos(9.*x)
-        };
-        Activation{
-            function:f,
-            derivation:d,
+    pub fn softmax() -> Self {
+        Activation {
+            function: |x: &f32| -> f32 {
+                if *x > 0. {
+                    return *x;
+                } else {
+                    0.
+                }
+            },
+            derivation: |x: &f32| -> f32 {
+                if *x > 0. {
+                    return 1.0;
+                } else {
+                    0.
+                }
+            },
         }
     }
-    pub fn sin()->Self{
-        Activation { function: |x:&f32|->f32{
-            x.sin()
-        }, derivation: |x:&f32|->f32{
-                x.cos()
-            } }
+
+    /// x^3+1+(sin 9x)*0.1 function
+    pub fn experimental_x3() -> Self {
+        let f = |x: &f32| -> f32 { x.powi(3) + 1. + f32::sin(9. * x) * 0.1 };
+        let d = |x: &f32| -> f32 { 3. * x.powi(2) + f32::cos(9. * x) };
+        Activation {
+            function: f,
+            derivation: d,
+        }
     }
-    pub fn abs_sin()->Self{
-        Activation { function: |x:&f32|->f32{
-            x.sin().abs()
-        }, derivation: |x:&f32|->f32{
-                if x.sin()<=0.{
-                    return 0.
+    pub fn sin() -> Self {
+        Activation {
+            function: |x: &f32| -> f32 { x.sin() },
+            derivation: |x: &f32| -> f32 { x.cos() },
+        }
+    }
+    pub fn abs_sin() -> Self {
+        Activation {
+            function: |x: &f32| -> f32 { x.sin().abs() },
+            derivation: |x: &f32| -> f32 {
+                if x.sin() <= 0. {
+                    return 0.;
                 }
                 x.cos()
-            } }
-
+            },
+        }
     }
-    
 }
